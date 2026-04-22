3ec70e4949679830c65389807d011d8b8e0cf5a3
hassanibnahmad <hassanibnahmad6@email.com>
2026-04-22 04:32:48 +0100

    Test CI/CD automation - Version 6

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 5 - Fully Automated CI/CD!"
+    return "Version 6 - CI/CD Pipeline Test!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
