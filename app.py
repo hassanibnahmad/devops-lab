edbbadc2c99f76760e64d2aa4056e1001c265dc9
hassanibnahmad <hassanibnahmad6@email.com>
2026-04-22 05:05:54 +0100

    Test complete pipeline - Version 9

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 8 - Docker Updates Automatically!"  
+    return "Version 9 - CI/CD Complete!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
