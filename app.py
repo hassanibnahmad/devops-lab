399c957798bdb5bbfdb1b6f22b44e5559e182567
hassanibnahmad <hassanibnahmad6@email.com>
2026-04-22 05:35:18 +0100

    Test fixed pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-     return "Version 10 - Final Pipeline!"
+     return "Version 10 - Final Pipeline!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
