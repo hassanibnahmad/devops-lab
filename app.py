c46a56fb644b1888e1ab9849d48d70bbf20971dd
hassanibnahmad <hassanibnahmad6@email.com>
2026-04-23 18:09:26 +0100

    test dans la classe v-3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-     return "Version 12 - test dans la classe !!! v-2"
+     return "Version 12 - test dans la classe !!! v-3"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
